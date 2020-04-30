580821c5786fca562cf52ababcf06d28395087d7
Daniel Pereira <danielpp95@outlook.com>
2020-05-01 00:49:26 +0100

    added header and file-loader

---
+++ src/components/Header.svelte
@@ -0,0 +1,80 @@
+<style>
+    .Header {
+        background-color: rgb(250, 155, 186);
+        position: fixed;
+        width: 100%;
+        color: black;
+    }
+
+    .Header-container {
+        display: grid;
+        grid-template-columns: minmax(auto, 936px);
+        justify-content: center;
+        border-bottom: 1px solid rgb(248, 128, 168)
+    }
+
+    .Header-content {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 10px;
+    }
+
+    .Header ul {
+        list-style: none;
+    }
+
+    .Header li {
+        display: inline-block;
+    }
+
+    .Header i {
+        font-size: 16px;
+        color: rgba(38, 38, 38, 0.8);
+        cursor: pointer;
+    }
+
+    h1 {
+        font-family: 'rochester';
+        font-size: 28px;
+        color: black;
+        cursor: pointer;
+        position: relative;
+        background: #fff;
+        mix-blend-mode: multiply;
+        display: inline-block;
+    }
+
+    h1:before {
+        content: "";
+        display: block;
+        width: 100%;
+        height: 100%;
+        background: radial-gradient(circle at top left,
+            rgba(14,0,36,1) 0%,
+            rgba(102,9,121,1) 33%,
+            rgba(255,0,96,1) 100%);
+        mix-blend-mode: screen;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+    }
+</style>
+
+<div class="Header">
+    <div class="Header-container">
+        <div class="Header-content">
+            <div class="Header-logo">
+                <h1>Sakura Card Captors</h1>
+                <!-- <img src={url} alt=""> -->
+            </div>
+            <div class="Header-nav">
+                <ul>
+                    <li>
+                        <i class="fas fa-plus"></i>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,16 +1,24 @@
 <script>
-    import CardForm from './CardForm.svelte'
+    import Header from '../components/Header.svelte'
 </script>
 
 <div class="App">
-    <CardForm />
+    <!-- <CardForm /> -->
+    <Header />
 </div>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap');
-
+    @import url('https://fonts.googleapis.com/css2?family=Rochester&display=swap');
+    
     :global(body) {
         font-family: 'Lato', sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: rgb(255, 184, 204);
+    }
+
+    :global(h1, h2, h3) {
         margin: 0;
         padding: 0;
     }
